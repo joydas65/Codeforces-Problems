030c0d9a9cb13c86a784ae700cdc3b357cfaf53b
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-11-29 17:49:20 +0530

    Create Way_Too_Long_Words.py

---
+++ Way_Too_Long_Words.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    
+    s = input()
+    
+    n = len(s)
+    
+    if n > 10:
+        
+        print(s[0]+str(n-2)+s[-1])
+        
+    else:
+        
+        print(s)
