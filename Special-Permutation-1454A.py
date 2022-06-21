8b1d9d0a25ed31c35e7fde94c18c721ce1774326
Joy Das <35962760+joydas65@users.noreply.github.com>
2022-06-21 21:30:58 +0530

    Create Special-Permutation-1454A.py

---
+++ Special-Permutation-1454A.py
@@ -0,0 +1,11 @@
+for _ in range(int(input())):
+    
+    n = int(input())
+        
+    print(n, end = " ")
+        
+    for i in range(1,n):
+            
+        print(i, end = " ")
+        
+    print()
